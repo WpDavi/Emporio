2f951d4226fddaa210c310fb2df6d75d1acc1c8b
WpDavi <100159858+WpDavi@users.noreply.github.com>
2023-08-06 22:52:32 -0300

    at

---
+++ src/routes/MainStack.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Login from "../screens/Login";
+import Registration from "../screens/Registration/Registration";
+import PreLogin from "../screens/PreLogin";
+
+import MainTab from "./MainTab";
+
+
+const Stack = createNativeStackNavigator()
+
+export default function MainStack() {
+    return(
+        <Stack.Navigator
+        screenOptions={{
+            headerShown: false
+        }}>                      
+            <Stack.Screen name="Login" component={Login} />
+            <Stack.Screen name="Registration" component={Registration} />
+            <Stack.Screen name="MainTab" component={MainTab} />
+
+        </Stack.Navigator>
+    )
+}
